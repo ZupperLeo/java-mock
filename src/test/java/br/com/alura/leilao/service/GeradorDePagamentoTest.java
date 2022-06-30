d00a57a89a421334e239a72dbce4d63e46c9d4d2
LeonardoZup <leonardo.araujo@zup.com.br>
2022-06-30 16:27:51 -0300

    Lidando com metodos estaticos

--- src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -4,7 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -27,30 +30,74 @@ class GeradorDePagamentoTest {
 	@Mock
 	private PagamentoDao dao;
 	
+	@Mock
+	private Clock clock;
+
 	@Captor
 	private ArgumentCaptor<Pagamento> captor;//Captura um objeto criado em um metodo sem retorno
 	
 	@BeforeEach
 	public void beforeEach() {
 		MockitoAnnotations.initMocks(this);//Inicia todos os atributos anotados com @Mock
-		this.service = new GeradorDePagamento(dao); 
+		this.service = new GeradorDePagamento(dao, clock); 
 	}
 	
 	@Test
-	void deveriaCriarPagamentoParaVencedorDoLeilao() {
+	void deveriaCriarPagamentoParaVencedorDoLeilaoEmDiaDasmena() {
 		Leilao leilao = leilao();
 		Lance vencedor = leilao.getLanceVencedor();
-		service.gerarPagamento(vencedor);
+		LocalDate data = LocalDate.of(2022, 7, 4);		
+		Instant instant = data.atStartOfDay(ZoneId.systemDefault()).toInstant();
 		
+		Mockito.when(clock.instant()).thenReturn(instant);
+		Mockito.when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+
+		service.gerarPagamento(vencedor);
 		Mockito.verify(dao).salvar(captor.capture());
 		
 		Pagamento pagamento = captor.getValue();
 		
-		assertEquals(LocalDate.now().plusDays(1), pagamento.getVencimento());
+		assertEquals(LocalDate.now(clock).plusDays(1), pagamento.getVencimento());
 		assertEquals(vencedor.getValor(), pagamento.getValor());
 		assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
 		assertEquals(leilao, pagamento.getLeilao());
 		assertFalse(pagamento.getPago());
+	}
+
+	@Test
+	void deveriaCriarPagamentoParaVencedorDoLeilaoNaSegundaQuandoOPagamentoCairNoSabado() {
+		Leilao leilao = leilao();
+		Lance vencedor = leilao.getLanceVencedor();
+		LocalDate data = LocalDate.of(2022, 6, 1);		
+		Instant instant = data.atStartOfDay(ZoneId.systemDefault()).toInstant();
+		
+		Mockito.when(clock.instant()).thenReturn(instant);
+		Mockito.when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+		
+		service.gerarPagamento(vencedor);
+		Mockito.verify(dao).salvar(captor.capture());
+		
+		Pagamento pagamento = captor.getValue();
+		
+		assertEquals(LocalDate.now(clock).plusDays(1), pagamento.getVencimento());
+	}
+
+	@Test
+	void deveriaCriarPagamentoParaVencedorDoLeilaoNaSegundaQuandoOPagamentoCairNoDomingo() {
+		Leilao leilao = leilao();
+		Lance vencedor = leilao.getLanceVencedor();
+		LocalDate data = LocalDate.of(2022, 6, 2);		
+		Instant instant = data.atStartOfDay(ZoneId.systemDefault()).toInstant();
+		
+		Mockito.when(clock.instant()).thenReturn(instant);
+		Mockito.when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+		
+		service.gerarPagamento(vencedor);
+		Mockito.verify(dao).salvar(captor.capture());
+		
+		Pagamento pagamento = captor.getValue();
+		
+		assertEquals(LocalDate.now(clock).plusDays(1), pagamento.getVencimento());
 	}
 
 	private Leilao leilao() {		
